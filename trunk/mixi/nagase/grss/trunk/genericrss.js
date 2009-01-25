c582b42ce852abc3766f494ea813f1fa3ea47b22
atsn.ngs <atsn.ngs@0009bb96-7749-0410-9aad-4d85576fd8e9>
2009-01-25 09:38:04 +0000

    ロジックはできた

--- trunk/mixi/nagase/grss/trunk/genericrss.js
+++ trunk/mixi/nagase/grss/trunk/genericrss.js
@@ -9,10 +9,12 @@ $.gadgets.ready(function(){
     "MAX_ENTRIES":"maxentries"
   };
   var MAX_FEED_SIZE = 10;
+  var MAX_ENTRY_SIZE = 10;
   var wrapper = $("#wrapper");
   var feeds = [];
   var feeds_ul = $("#pref ul.feeds");
   var cue, entries;
+  var entries_ul = $("#list ul.entries");
   $("p.go-pref span.link").bind("click",prefView);
   $("p.add-feed span.link").bind("click",appendInput);
   $("#submit-feeds-button").bind("click",acceptPref);
@@ -29,7 +31,7 @@ $.gadgets.ready(function(){
     feeds = feeds || [];
     $("#pref ul.feeds").empty();
     $.each(feeds,function(){
-      appendInput(this.toString());
+      appendInput(this.URL.toString());
     });
   }
 
@@ -46,7 +48,7 @@ $.gadgets.ready(function(){
     var li = $("li",feeds_ul), i = li.size();
     if(i==MAX_FEED_SIZE) return error("フィードは"+MAX_FEED_SIZE+"件までしか登録できません。");
     feeds_ul.append([
-      "<li class=\"item\" id=\"feed",i,"\">",
+      "<li class=\"item ",i%2?"odd":"even","\" id=\"feed",i,"\">",
         "<input type=\"text\" value=\"",url,"\" id=\"feedurl",i,"\" \/>",
         "<span class=\"link remove\">X<\/span>",
       "<\/li>"
@@ -62,7 +64,7 @@ $.gadgets.ready(function(){
     feeds = [];
     $("li.item input",feeds_ul).each(function(){
       var val = $(this).val();
-      if(isValidURL(val)) feeds.push(val);
+      if(isValidURL(val)) feeds.push({ URL:val });
     });
     if(feeds.length) {
       $.opensocial.data.set(PrefKey.FEEDS,feeds,function(success){
@@ -78,24 +80,55 @@ $.gadgets.ready(function(){
 
   function getFeed() {
     if(!feeds[cue]) return onCompleteFeed();
-    $.gadgets.getFeed(feeds[cue],function(d){
-      if(d) {
-        $.each(d,function(){
-          entries.push({ entry:this,title:d.Title,link:d.Link,URL:d.URL });
+    $.gadgets.getFeed(feeds[cue].URL,{},function(d){
+      feeds[cue] = $.extend(feeds[cue],d[0]);
+      if(d&&d[0]&&d[0].Entry) {
+        $.each(d[0].Entry,function(){
+          entries.push($.extend({ Feed:cue },this));
         });
       }
+      cue++; 
       getFeed();
-    },MAX_FEED_SIZE);
-    cue++; 
+    },MAX_ENTRY_SIZE,true);
   }
 
   function onCompleteFeed() {
-    console.log(entries);
+    entries = sortEntries(entries);
+    if(!entries||!entries.length) return error("エントリーが1件もありません。");
+    var ht = [];
+    $.each(entries,function(i){
+      if(i>=MAX_ENTRY_SIZE) return false;
+      var f = feeds[this.Feed];
+      ht.push([
+        "<li class=\"entry ",i%2?"odd":"even","\">",
+          "<p class=\"head\">",
+          "<span class=\"date\">",getDateString(this.Date),"<\/span>",
+          "<strong class=\"title\"><a href=\"",this.Link,"\" target=\"_blank\">",this.Title,"<\/a><\/strong>",
+          "<span class=\"site\"><a href=\"",f.Link,"\" target=\"_blank\">",f.Title,"<\/a><\/span>",
+          "<\/p>",
+          this.Summary?("<blockquote>"+this.Summary+"<\/blockquote>"):"",
+        "<\/li>"
+      ].join(""));
+    });
+    entries_ul.html(ht.join(""));
+    $.gadgets.height("auto");
+  }
+
+  function getDateString(t) {
+    var date = new Date(t*1000);
+    var today = new Date();
+    if(date.getDate()==today.getDate())
+      return [format2Digs(date.getHours()),":",format2Digs(date.getMinutes())].join("");
+    return [format2Digs(date.getMonth()+1),"\/",format2Digs(date.getDate())].join("");
+  }
+
+  function format2Digs(n) {
+    return n<10?"0"+n.toString():n.toString();
   }
 
   function sortEntries(ar) {
     return ar.sort(function(a,b){
-      
+      return b.Date-a.Date;
     });
   }
 
